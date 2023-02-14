d80e6bb615c2c46f36e93eeb765b43ba6562108d
matthew-hre <whycardboard@gmail.com>
2023-02-14 13:12:38 -0700

    adjusted commit time to mst

--- src/Footer.js
+++ src/Footer.js
@@ -10,9 +10,18 @@ function Footer() {
       .get(`https://api.github.com/repos/matthew-hre/matthew-hre/commits`)
       .then((response) => {
         const commitSha = response.data[0].sha;
-        const commitTime = response.data[0].commit.committer.date
+        let commitTime = response.data[0].commit.committer.date
           .replace("T", " at ")
           .replace("Z", " ");
+
+        let splittedTime = commitTime.split(" at ");
+        let time = splittedTime[1].split(":");
+        let hour = parseInt(time[0]);
+        time[0] = hour - 7 < 0 ? hour + 17 : hour - 7;
+        time = time.join(":");
+        splittedTime[1] = time;
+        commitTime = splittedTime.join(" at ");
+
         setCommit({
           sha: commitSha,
           time: commitTime,
