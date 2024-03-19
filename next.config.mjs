64dc2c77ba6e02851c8ed59c018dc27bbe45f3be
matthew-hre <whycardboard@gmail.com>
2024-03-18 20:06:22 -0600

    fixed the wicked nest of bugs that was my attempt at mdx implementation, god forgive me

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import NextMdx from "@next/mdx";
-
-import remarkGfm from "remark-gfm";
-import rehypeSlug from "rehype-slug";
-
-const withMdx = NextMdx({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeSlug],
-  },
-});
-
-const nextConfig = withMdx({
-  pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
