e7e4c1439497a06314dd0818df6c06e2066dc349
matthew-hre <whycardboard@gmail.com>
2024-03-30 15:22:20 -0600

    reworked the static param generation again

--- app/[...path]/page.tsx
+++ app/[...path]/page.tsx
@@ -17,41 +17,52 @@ export default async function Page({
   return <ArticlePage params={params} />;
 }
 
+// export async function generateStaticParams() {
+//   const learningSlugs = await reader.collections.learning.list();
+
+//   const learningPaths = learningSlugs.map((slug) => ({
+//     params: {
+//       path: ["learning", slug],
+//     },
+//   }));
+
+//   const blogSlugs = await reader.collections.blog.list();
+
+//   const blogPaths = blogSlugs.map((slug) => ({
+//     params: {
+//       path: ["blog", slug],
+//     },
+//   }));
+
+//   const projectSlugs = await reader.collections.projects.list();
+
+//   const projectPaths = projectSlugs.map((slug) => ({
+//     params: {
+//       path: ["projects", slug],
+//     },
+//   }));
+
+//   const gamesSlugs = await reader.collections.games.list();
+
+//   const gamePaths = gamesSlugs.map((slug) => ({
+//     params: {
+//       path: ["games", slug],
+//     },
+//   }));
+
+//   return [...learningPaths, ...blogPaths, ...projectPaths, ...gamePaths];
+// }
+
 export async function generateStaticParams() {
-  const learningSlugs = await reader.collections.learning.list();
+  const blogPaths = await reader.collections.blog.list();
+  const learningPaths = await reader.collections.learning.list();
+  const projectPaths = await reader.collections.projects.list();
+  const gamePaths = await reader.collections.games.list();
 
-  const learningPaths = learningSlugs.map((slug) => ({
-    params: {
-      path: ["learning", slug],
-    },
-  }));
-
-  const blogSlugs = await reader.collections.blog.list();
-
-  const blogPaths = blogSlugs.map((slug) => ({
-    params: {
-      path: ["blog", slug],
-    },
-  }));
-
-  const projectSlugs = await reader.collections.projects.list();
-
-  const projectPaths = projectSlugs.map((slug) => ({
-    params: {
-      path: ["projects", slug],
-    },
-  }));
-
-  const gamesSlugs = await reader.collections.games.list();
-
-  const gamePaths = gamesSlugs.map((slug) => ({
-    params: {
-      path: ["games", slug],
-    },
-  }));
-
-  console.log("Generated paths for pages");
-  console.log([...learningPaths, ...blogPaths, ...projectPaths, ...gamePaths]);
-
-  return [...learningPaths, ...blogPaths, ...projectPaths, ...gamePaths];
+  return [
+    ...blogPaths.map((slug) => ({ path: ["blog", slug] })),
+    ...learningPaths.map((slug) => ({ path: ["learning", slug] })),
+    ...projectPaths.map((slug) => ({ path: ["projects", slug] })),
+    ...gamePaths.map((slug) => ({ path: ["games", slug] })),
+  ];
 }
