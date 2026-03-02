79b395cc4c501cac5779c934f64891138ab4df65
Matthew Hrehirchuk <me@matthew-hre.com>
2026-03-01 21:55:16 -0700

    next: move to react compiler, eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   pageExtensions: ["ts", "tsx", "mdx"],
   images: {
     remotePatterns: [
